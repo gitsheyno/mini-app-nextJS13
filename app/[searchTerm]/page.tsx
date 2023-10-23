a5ac7b3f3ad717366ed38030b67e0da01aa60cc8
shayan heidary <hdrydeveloper@gmail.com>
2023-10-23 18:09:56 +0200

    app init client and server compnent implemented and dynamic metadata

---
+++ app/[searchTerm]/page.tsx
@@ -0,0 +1,46 @@
+import getWikiResults from "@/lib/getWikiRes";
+type Props = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+
+  const displayTerm = searchTerm.replaceAll("%20", " ");
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${displayTerm} not found`,
+    };
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search result for ${displayTerm}`,
+  };
+}
+
+export default async function SearchResultPage({
+  params: { searchTerm },
+}: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+
+  const resault: Result[] | undefined = data?.query?.pages;
+
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 main-h-screen">
+      {resault ? (
+        Object.values(resault).map((result) => (
+          <p key={result.pageid}>{JSON.stringify(result)}</p>
+        ))
+      ) : (
+        <h2>`${searchTerm} is not found`</h2>
+      )}
+    </main>
+  );
+  return content;
+}
---
+++ app/components/Search.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+
+    router.push(`/${search}/`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-50 flex justify-center md:justy-between"
+    >
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="bg-white p-2 w-80 text-xl rounded-xl"
+        placeholder="Search"
+      />
+      <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+        🚀
+      </button>
+    </form>
+  );
+}
+
+export default Search;
